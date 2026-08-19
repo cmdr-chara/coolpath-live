a981ab803435df8e8ba2d49d54886a7efc616e4c
Chara <legend.alkalex07@gmail.com>
2026-08-19 17:49:00 +0200

    Export canonical PA211 source policy

--- packages/source-adapters/src/index.ts
+++ packages/source-adapters/src/index.ts
@@ -4,5 +4,6 @@ export * from "./fresno-normalizer.js";
 export * from "./heal-prompt.js";
 export * from "./mock-client.js";
 export * from "./pa211-normalizer.js";
+export * from "./pa211-source.js";
 export * from "./source-manifest.js";
 export * from "./types.js";
